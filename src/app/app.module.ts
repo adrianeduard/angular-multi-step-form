9e3bdfd69c8d5d5393ae0b72b66e54690632f4c6
adrianeduard <adrian.eduard@gmail.com>
2022-08-11 13:06:12 +0300

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule,
+    RouterModule.forRoot([]),
     CoreModule,
     PersonalModule,
     AppRoutingModule,
